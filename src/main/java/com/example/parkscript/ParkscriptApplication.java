ca906e1fc20d7f28409d7913303bd91ed46ea5c5
Yudi Matsuguma <yudimatsuguma@gmail.com>
2024-10-08 08:20:37 -0300

    initial files

---
+++ src/main/java/com/example/parkscript/ParkscriptApplication.java
@@ -0,0 +1,11 @@
+package com.example.parkscript;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkscriptApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ParkscriptApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/parkscript/ParkscriptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parkscript;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkscriptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
