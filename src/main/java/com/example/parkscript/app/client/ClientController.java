1e4ae6d6ee0767c265c8c21fcccb3bbbc19dc442
Yudi Matsuguma <yudimatsuguma@gmail.com>
2024-10-10 01:11:37 -0300

    refactor: renomeando rota de procurar cliente pelo cpf

--- src/main/java/com/example/parkscript/app/client/ClientController.java
+++ src/main/java/com/example/parkscript/app/client/ClientController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 public class ClientController {
     private final FindClientByCpfUseCase findClientByCpfUseCase;
 
-    @PostMapping()
+    @PostMapping("/find-by-cpf")
     public ResponseEntity<ClientDto> findByCpf(@RequestBody @Valid FindClientByCpfInputDto body) {
         var result = this.findClientByCpfUseCase.execute(body.cpf());
 
